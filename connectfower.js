4585acea9c088349f5b664783b3aa1a535fa35a5
iankantian <iankantian@gmail.com>
2016-03-15 13:30:02 -0700

    now can drop chips

--- connectfower.js
+++ connectfower.js
@@ -4,30 +4,62 @@
 /*
 * connect four game
  */
+
+var black = 1;
+var red = 2;
+
 var playArea = [
-    [ false,false,false,false,false,false,false ],
-    [ false,false,false,false,false,false,false ],
-    [ false,false,false,false,false,false,false ],
-    [ false,false,false,false,false,false,false ],
-    [ false,false,false,false,false,false,false ],
-    [ false,false,false,false,false,false,false ]
+    [ 0,0,0,0,0,0,0 ],
+    [ 0,0,0,0,0,0,0 ],
+    [ 0,0,0,0,0,0,0 ],
+    [ 0,0,0,0,0,0,0 ],
+    [ 0,0,0,0,0,0,0 ],
+    [ 0,0,0,0,0,0,0 ]
 ];
 
 function displayPlayArea( inputArray ){
     //console.log(playArea); // too ugly
     // this works, however, it be nicer to do something a bit more colorful with SVG.
+    console.log('current state of play area');
     for( var i = 0; i < inputArray.length; i++ ){
         console.log( inputArray[i] );
     }
 }
 
-function takeTurn( inputArray ){
+function randomColumn (){
+    var result = Math.floor( ( Math.random() * 7 ) + 0 );
+    if ( result == 7 ){
+        result--;
+    }
+    return result;
+}
+
+function takeTurn( color, inputArray ){
+    if( inputArray == undefined ){
+        inputArray = playArea;
+    }
     // this will be in random mode.
-    var targetColumn = 0;
-    targetColumn = Math.floor( ( Math.random() * 7 ) + 1 );
+    var targetColumn = randomColumn();
     //console.log( targetColumn );
-    for( var i = 0; i < inputArray.length; i++ ){
-        inputArray[ i ][ targetColumn ] = true;
+    for( var i = 0; i < inputArray.length; i++ ){ // row at a time
+        if( inputArray[ i ][ targetColumn ] == black || inputArray[ i ][ targetColumn ] == red ){
+            console.log( 'found checker ', color );
+            if( i == 0 ){
+                //need to try another column, this one's all filled.
+            }
+            else{
+                inputArray[ i - 1 ][ targetColumn ] = color;
+            }
+        }
+        else if( i == inputArray.length - 1 ){
+                //bottom of input array, place checker
+                console.log( 'bottom found! placing color ', color);
+                inputArray[ i ][ targetColumn ] = color;
+                break;
+        }
+        else{
+            console.log('else');
+        }
     }
     displayPlayArea( inputArray );
 }
@@ -38,4 +70,4 @@ function checkWin(){
 
 // initial display
 displayPlayArea(playArea);
-takeTurn( playArea );
+takeTurn( black );
